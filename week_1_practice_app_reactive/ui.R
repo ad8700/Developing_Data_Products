a62aff8151ad06563738cc3065058beecb419036
Domenico <andrew.domenico@accenture.com>
2022-08-20 21:02:26 -0500

    Using reactive within app

---
+++ week_1_practice_app_reactive/ui.R
@@ -0,0 +1,21 @@
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  titlePanel("Predict Horsepower from MPG"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("sliderMPG", "What is the MPG of the car?", 10,35,value=20),
+      checkboxInput("showModel1", "Show/Hide Model 1", value=TRUE),
+      checkboxInput("showModel2", "Show/Hide Model 2", value=TRUE)
+      ),
+    mainPanel(
+      plotOutput("plot1"),
+      h3("Predicted horsepower from Model 1: "),
+      textOutput("pred1"),
+      h3("Predicted horsepower from Model 2: "),
+      textOutput("pred2")
+    )
+  )
+))
